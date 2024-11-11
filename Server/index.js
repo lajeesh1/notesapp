574db65db224bea0c1c53dd8f896b9e75a48e210
Lajeesh <gokulam.9920@gmail.com>
2024-11-11 20:13:52 +0530

    initial commit 2

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,7 @@
-import express from 'express';
-
+import express from "express";
 
 const app = express();
 
-app.listen(5000,()=>{
-    console.log('server is running...uu.')
-})
+app.listen(5000, () => {
+  console.log("server is running...uu....");
+});
